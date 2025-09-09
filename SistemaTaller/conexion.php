76c1ba0ad2d4cf653c99da05597cf8830e2e0e33
Gerardo A. <74476285+gerardoslzr@users.noreply.github.com>
2025-09-09 09:56:12 -0600

    Add files via upload
    Template ADMINLTE inplementado al 100% listo para usar

---
+++ SistemaTaller/conexion.php
@@ -0,0 +1,44 @@
+<?php
+// ESTAS SON LAS VARIABLES GLOBALES PARA SOLO REFERENCIARLAS EN LA FUNCION QUE CONECTA LA BASE DE DATOS
+DEFINE("SERVER", "localhost");
+DEFINE("USUARIO", "root");
+DEFINE("PASS", "");
+DEFINE("BASE", "taller_gerardo");
+//DEFINE("URL_FRONT","http://localhost/");
+
+// ESTA FUNCION ES LA USARAS MULTIPLES VECES PARA CONECTARTE DE MANERA SEGURA A LA DB - PRIMERO EMPIEZA CON MYSQLi LUEGO PASA A PDO
+
+function conectar(){
+	$con = @mysqli_connect(SERVER, USUARIO, PASS, BASE);
+    $error = mysqli_error($con);
+	if(!$con){
+		die("imposible conectarse: ".$error);
+	}
+	if (@mysqli_connect_errno()) {
+		die("Conexion Fallo: ".mysqli_connect_errno()." : ". mysqli_connect_error());
+	}
+	$query = "set names utf8";
+	$resultado=$con->query($query);
+	return $con;
+	$con->close();
+}
+function mostrarErrores() {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+function check_session(){
+	session_start();
+		if(!isset($_SESSION['id_usuario'])){
+		header("Location: login.php"); 
+	}
+}
+function check_sesion_admin(){
+	session_start();
+	if(isset($_SESSION['id_usuario'])){
+			header("Location: index.php");
+	}
+}
+
+?>
